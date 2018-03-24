ad734311f95a7d913c637a10bf655c2270504cb7
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2018-03-24 20:01:13 +0200

    integrate EJS template engine example

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ app.use(express.static('public'));
 //app.use(express.static('src/views'));
 app.set('views', './src/views');
 //app.set('view engine', 'jade');
-var handlebars = require('express-handlebars');
-app.engine('.hbs', handlebars({extname: '.hbs'}));
-app.set('view engine', '.hbs');
+// var handlebars = require('express-handlebars');
+// app.engine('.hbs', handlebars({extname: '.hbs'}));
+// app.set('view engine', '.hbs');
+
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index', {title: 'Hello from render', list: ['a', 'b']});
+    res.render('index', {title: 'Hello from render!', list: ['a', 'b', 'c']});
 });
 
 app.listen(port, function (err) {
