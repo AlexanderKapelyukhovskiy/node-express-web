750ec3a8e1d9f3d0c3b98dfd52df6e15fbb55ce0
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2018-03-24 20:06:05 +0200

    integrate EJS to main template and remove previous examples files and links

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-//app.use(express.static('src/views'));
 app.set('views', './src/views');
-//app.set('view engine', 'jade');
-// var handlebars = require('express-handlebars');
-// app.engine('.hbs', handlebars({extname: '.hbs'}));
-// app.set('view engine', '.hbs');
 
 app.set('view engine', 'ejs');
 
