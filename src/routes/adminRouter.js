eec370d07c8146ecca7a17ab95a43514decb92a9
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2018-03-29 23:27:24 +0300

    get book details from goodreads api

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -7,12 +7,14 @@ var books = [
     title: 'War and Peace',
     ganre: 'Historial Fiction',
     author: 'Lev Nikolaevich Tolstoy',
+    bookId: 656,
     read: false
   },
   {
     title: 'Les Miserables',
     ganre: 'Historial Fiction',
     author: 'Vitor Hugo',
+    bookId: 24280,
     read: false
   },
   {
@@ -38,8 +40,8 @@ var router = function (navs) {
 
   adminRouter.route('/addBooks')
     .get(function (req, res) {
-      //var url = 'mongodb://localhost:27017/libraryApp';
-      var url = 'mongodb://alkapa:alkapamongodb@cluster0-shard-00-00-xxzqp.mongodb.net:27017,cluster0-shard-00-01-xxzqp.mongodb.net:27017,cluster0-shard-00-02-xxzqp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+      var url = 'mongodb://localhost:27017/libraryApp';
+      //var url = 'mongodb://alkapa:alkapamongodb@cluster0-shard-00-00-xxzqp.mongodb.net:27017,cluster0-shard-00-01-xxzqp.mongodb.net:27017,cluster0-shard-00-02-xxzqp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
       //mongodb://alkapa:alkapamongodb@cluster0-shard-00-00-xxzqp.mongodb.net:27017,cluster0-shard-00-01-xxzqp.:27017,cluster0-shard-00-02-xxzqp.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin
       mongodb.connect(url, function (err, database) {
         if(err) {
--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,7 +1,21 @@
- function goodreadsService() {
-  function getBookById() {
+var axios = require('axios');
+var xml2js = require('xml2js');
+
+var parser = xml2js.Parser({explicitArray: false});
+
+function goodreadsService() {
+  function getBookById(bookId) {
     return new Promise(function(resolve, reject){
-      resolve({description: 'our description'});
+      var url = `https://www.goodreads.com/book/show/${bookId}.xml?key=sgidlTrK0Ou4aW1mJeo3pQ`;
+      axios.get(url)
+      .then(function(response){
+        parser.parseString(response.data, function(error, result) {
+          resolve(result.GoodreadsResponse.book);
+        });
+      })
+      .catch(function(e){
+        reject(e);
+      });
     });
   }
 
